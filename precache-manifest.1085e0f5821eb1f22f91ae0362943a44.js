231ce47be726a788241644df79d72926526eec6b
Luis Zaman <zamanlh@umich.edu>
2021-08-26 15:14:44 -0400

    Updates

---
+++ precache-manifest.1085e0f5821eb1f22f91ae0362943a44.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2150ed8fa1e19e5330d6e591bc009f8",
+    "url": "/NIH-Modules/index.html"
+  },
+  {
+    "revision": "995ac6c3114f2684e409",
+    "url": "/NIH-Modules/static/css/main.570b9990.chunk.css"
+  },
+  {
+    "revision": "8c724a95360c9688b636",
+    "url": "/NIH-Modules/static/js/2.53fef568.chunk.js"
+  },
+  {
+    "revision": "f8922b16413369046bb8809cc138c042",
+    "url": "/NIH-Modules/static/js/2.53fef568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995ac6c3114f2684e409",
+    "url": "/NIH-Modules/static/js/main.cdde4c61.chunk.js"
+  },
+  {
+    "revision": "c60c151227927bbe9d53",
+    "url": "/NIH-Modules/static/js/runtime-main.32b90157.js"
+  },
+  {
+    "revision": "e890612160606ac018b6e9cf80960b3f",
+    "url": "/NIH-Modules/static/media/elizabeth.e8906121.jpg"
+  },
+  {
+    "revision": "a98b9e9973464696175736b7ccfd926d",
+    "url": "/NIH-Modules/static/media/emily.a98b9e99.jpg"
+  },
+  {
+    "revision": "bf6ed4cd8ca3d953466790a246cd76c1",
+    "url": "/NIH-Modules/static/media/jon.bf6ed4cd.jpg"
+  },
+  {
+    "revision": "701b90fb531bf4186a77d7e815a0b1bb",
+    "url": "/NIH-Modules/static/media/luis.701b90fb.jpg"
+  },
+  {
+    "revision": "1718f0fd5485025b5559ccf35209c2c1",
+    "url": "/NIH-Modules/static/media/m12_preview.1718f0fd.png"
+  },
+  {
+    "revision": "026a3452a88b404c80936aaa1f38244a",
+    "url": "/NIH-Modules/static/media/marisa.026a3452.png"
+  },
+  {
+    "revision": "d2b4d7da95401ce8ba494c1ffa3d6879",
+    "url": "/NIH-Modules/static/media/mod3screen.d2b4d7da.jpg"
+  },
+  {
+    "revision": "4813ff1d61ce07c8e7c73a5216ee918e",
+    "url": "/NIH-Modules/static/media/output.4813ff1d.mp4"
+  },
+  {
+    "revision": "e2902f3aaa5289cee2f08bba15ea931b",
+    "url": "/NIH-Modules/static/media/rnaught_preview.e2902f3a.png"
+  },
+  {
+    "revision": "80ce24b09c0e46733513264cbb035a70",
+    "url": "/NIH-Modules/static/media/stephanie.80ce24b0.png"
+  }
+]);
